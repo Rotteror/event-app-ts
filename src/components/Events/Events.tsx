1c0ab436f2be27017c0bc6149cefc4584f033944
Rotteror <xeziod@gmail.com>
2023-04-27 11:30:33 +0300

    feat(events): add events component

---
+++ src/components/Events/Events.tsx
@@ -0,0 +1,95 @@
+import "./events.css";
+
+const Events = () => {
+  return (
+    <div className="container">
+      <div className="event-container">
+        <div className="event-card">
+          <div className="image-wrap">
+            <img
+              src="https://images.prismic.io/terminal-27/44da8e30-4aeb-41fc-be6b-741e90f7af42_IMG_1892.jpg?auto=compress,format&rect=416,0,1632,1632&w=1000&h=1000"
+              alt=""
+            />
+          </div>
+          <div className="event-titles">
+            <h3>
+              <span>First Title</span>
+              <span>Second Title</span>
+            </h3>
+          </div>
+          <div className="event-content">
+            <p>
+              Lorem ipsium aka ,Lorem ipsium aka,Lorem ipsium aka ,Lorem ipsium
+              aka
+            </p>
+          </div>
+        </div>
+        {/* 2 */}
+        <div className="event-card">
+          <div className="image-wrap">
+            <img
+              src="https://images.prismic.io/terminal-27/57e9550b-3b01-4176-8f81-2a1dc50d712c_MERCH.png?auto=compress,format&rect=0,0,3000,3000&w=1000&h=1000&w=1920&h=1920"
+              alt=""
+            />
+          </div>
+          <div className="event-titles">
+            <h3>
+              <span>First Title</span>
+              <span>Second Title</span>
+            </h3>
+          </div>
+          <div className="event-content">
+            <p>
+              Lorem ipsium aka ,Lorem ipsium aka,Lorem ipsium aka ,Lorem ipsium
+              aka
+            </p>
+          </div>
+        </div>
+        {/* 3 */}
+        <div className="event-card">
+          <div className="image-wrap">
+            <img
+              src="https://images.prismic.io/terminal-27/7116c4e6-320f-4868-8d6d-63966fe41a85_328162586_724404192597726_2533259174265035343_n.jpg?auto=compress,format&rect=0,135,1080,1080&w=1000&h=1000&w=1920&h=1920"
+              alt=""
+            />
+          </div>
+          <div className="event-titles">
+            <h3>
+              <span>First Title</span>
+              <span>Second Title</span>
+            </h3>
+          </div>
+          <div className="event-content">
+            <p>
+              Lorem ipsium aka ,Lorem ipsium aka,Lorem ipsium aka ,Lorem ipsium
+              aka
+            </p>
+          </div>
+        </div>
+        {/* 4 */}
+        <div className="event-card">
+          <div className="image-wrap">
+            <img
+              src="https://images.prismic.io/terminal-27/04c798f8-77cc-4e9c-98ab-ba60078aabff_%D0%A1%D0%BA%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C212+%E2%80%94+%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F.png?auto=compress,format&rect=659,0,2617,2617&w=1000&h=1000&w=1920&h=1920"
+              alt=""
+            />
+          </div>
+          <div className="event-titles">
+            <h3>
+              <span>First Title</span>
+              <span>Second Title</span>
+            </h3>
+          </div>
+          <div className="event-content">
+            <p>
+              Lorem ipsium aka ,Lorem ipsium aka,Lorem ipsium aka ,Lorem ipsium
+              aka
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Events;
